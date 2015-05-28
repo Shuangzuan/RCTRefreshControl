dbce44c18cef6e720c4736d94890e0e444be3649
Shuangzuan <shuangzuan.he@icloud.com>
2015-05-28 11:14:48 +0800

    2.0.0

--- RCTRefreshControl.ios.js
+++ RCTRefreshControl.ios.js
@@ -4,16 +4,44 @@
  */
 'use strict';
 
+var React = require('react-native');
 var NativeRCTRefreshControl = require('NativeModules').RefreshControl;
 var invariant = require('invariant');
+var {
+  DeviceEventEmitter
+} = React;
 
 /**
- * High-level docs for the RCTRefreshControl iOS API can be written here.
+ * A pull down to refresh control like the one in Apple's iOS6 Mail App.
  */
 
+var DROP_VIEW_DID_BEGIN_REFRESHING_EVENT = 'dropViewDidBeginRefreshing';
+
+var callbacks = {};
+
+var subscription = DeviceEventEmitter.addListener(
+  DROP_VIEW_DID_BEGIN_REFRESHING_EVENT,
+  (reactTag) => callbacks[reactTag]()
+);
+// subscription.remove();
+
 var RCTRefreshControl = {
-  test: function() {
-    NativeRCTRefreshControl.test();
+  configure: function(configs, callback) {
+    var nodeHandle = React.findNodeHandle(configs.node);
+    var options = {
+      tintColor: configs.tintColor,
+      activityIndicatorViewColor: configs.activityIndicatorViewColor
+    };
+    
+    NativeRCTRefreshControl.configure(nodeHandle, options, (error) => {
+      if (!error) {
+        callbacks[nodeHandle] = callback;
+      }
+    });
+  },
+  endRefreshing: function(node) {
+    var nodeHandle = React.findNodeHandle(node);
+    NativeRCTRefreshControl.endRefreshing(nodeHandle);
   }
 };
 
--- RCTRefreshControl.m
+++ RCTRefreshControl.m
@@ -1,12 +1,109 @@
 #import "RCTRefreshControl.h"
 
+#import "ODRefreshControl.h"
+
+#import "RCTBridge.h"
+#import "RCTConvert.h"
+#import "RCTScrollView.h"
+#import "RCTSparseArray.h"
+#import "RCTUIManager.h"
+#import "RCTEventDispatcher.h"
+
 @implementation RCTRefreshControl
+
+#pragma mark -
+
+@synthesize bridge = _bridge;
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(test)
-{
-  // Your implementation here
+- (dispatch_queue_t)methodQueue {
+  return self.bridge.uiManager.methodQueue;
+}
+
+RCT_EXPORT_METHOD(configure:(NSNumber *)reactTag
+                  options:(NSDictionary *)options
+                  callback:(RCTResponseSenderBlock)callback) {
+  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
+    
+    UIView *view = viewRegistry[reactTag];
+    if (!view) {
+      RCTLogError(@"Cannot find view with tag #%@", reactTag);
+      return;
+    }
+    
+    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+    
+    ODRefreshControl *refreshControl = [[ODRefreshControl alloc] initInScrollView:scrollView];
+    refreshControl.tag = [reactTag integerValue]; // Maybe something better
+    
+    UIColor *tintColor = options[@"tintColor"];
+    // TODO: activityIndicatorViewStyle
+    UIColor *activityIndicatorViewColor = options[@"activityIndicatorViewColor"];
+    
+    if (tintColor) refreshControl.tintColor = [RCTConvert UIColor:tintColor];
+    if (activityIndicatorViewColor) refreshControl.activityIndicatorViewColor = [RCTConvert UIColor:activityIndicatorViewColor];
+    
+    [refreshControl addTarget:self action:@selector(dropViewDidBeginRefreshing:) forControlEvents:UIControlEventValueChanged];
+    
+    callback(@[[NSNull null], reactTag]);
+  }];
+}
+
+RCT_EXPORT_METHOD(beginRefreshing:(NSNumber *)reactTag) {
+  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
+    
+    UIView *view = viewRegistry[reactTag];
+    if (!view) {
+      RCTLogError(@"Cannot find view with tag #%@", reactTag);
+      return;
+    }
+    
+    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+    
+    ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [refreshControl beginRefreshing];
+    });
+  }];
+}
+
+RCT_EXPORT_METHOD(endRefreshing:(NSNumber *)reactTag) {
+  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
+    
+    UIView *view = viewRegistry[reactTag];
+    if (!view) {
+      RCTLogError(@"Cannot find view with tag #%@", reactTag);
+      return;
+    }
+    
+    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+    
+    ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+       [refreshControl endRefreshing];
+    });
+  }];
+}
+
+- (NSDictionary *)constantsToExport {
+  return @{@"UIActivityIndicatorViewStyleWhiteLarge": @(UIActivityIndicatorViewStyleWhiteLarge),
+           @"UIActivityIndicatorViewStyleWhite": @(UIActivityIndicatorViewStyleWhite),
+           @"UIActivityIndicatorViewStyleGray": @(UIActivityIndicatorViewStyleGray)};
+}
+
+- (void)dropViewDidBeginRefreshing:(ODRefreshControl *)refreshControl {
+  [self.bridge.eventDispatcher sendDeviceEventWithName:@"dropViewDidBeginRefreshing"
+                                                  body:@(refreshControl.tag)];
+  
+  /*
+  double delayInSeconds = 3.0;
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    [refreshControl endRefreshing];
+  }); */
 }
 
 @end
