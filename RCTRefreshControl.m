b1f9a20e03ce63e2644874f3a954e1f5b34f32a5
Parry <qiupengyuan@gmail.com>
2016-01-08 19:30:02 +0800

    fixed RCTSparseArray issues
    Fixed after React-Native 0.16.0: 'RCTSparseArray.h' file not found error.

--- RCTRefreshControl.m
+++ RCTRefreshControl.m
@@ -5,7 +5,6 @@
 #import "RCTBridge.h"
 #import "RCTConvert.h"
 #import "RCTScrollView.h"
-#import "RCTSparseArray.h"
 #import "RCTUIManager.h"
 #import "RCTEventDispatcher.h"
 
@@ -18,92 +17,92 @@
 RCT_EXPORT_MODULE()
 
 - (dispatch_queue_t)methodQueue {
-  return self.bridge.uiManager.methodQueue;
+    return self.bridge.uiManager.methodQueue;
 }
 
 RCT_EXPORT_METHOD(configure:(nonnull NSNumber *)reactTag
                   options:(NSDictionary *)options
                   callback:(RCTResponseSenderBlock)callback) {
-  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
-    
-    UIView *view = viewRegistry[reactTag];
-    if (!view) {
-      RCTLogError(@"Cannot find view with tag #%@", reactTag);
-      return;
-    }
-    
-    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
-    
-    ODRefreshControl *refreshControl = [[ODRefreshControl alloc] initInScrollView:scrollView];
-    refreshControl.tag = [reactTag integerValue]; // Maybe something better
-    
-    UIColor *tintColor = options[@"tintColor"];
-    // TODO: activityIndicatorViewStyle
-    UIColor *activityIndicatorViewColor = options[@"activityIndicatorViewColor"];
-    
-    if (tintColor) refreshControl.tintColor = [RCTConvert UIColor:tintColor];
-    if (activityIndicatorViewColor) refreshControl.activityIndicatorViewColor = [RCTConvert UIColor:activityIndicatorViewColor];
-    
-    [refreshControl addTarget:self action:@selector(dropViewDidBeginRefreshing:) forControlEvents:UIControlEventValueChanged];
-    
-    callback(@[[NSNull null], reactTag]);
-  }];
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary *viewRegistry) {
+        
+        UIView *view = viewRegistry[reactTag];
+        if (!view) {
+            RCTLogError(@"Cannot find view with tag #%@", reactTag);
+            return;
+        }
+        
+        UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+        
+        ODRefreshControl *refreshControl = [[ODRefreshControl alloc] initInScrollView:scrollView];
+        refreshControl.tag = [reactTag integerValue]; // Maybe something better
+        
+        UIColor *tintColor = options[@"tintColor"];
+        // TODO: activityIndicatorViewStyle
+        UIColor *activityIndicatorViewColor = options[@"activityIndicatorViewColor"];
+        
+        if (tintColor) refreshControl.tintColor = [RCTConvert UIColor:tintColor];
+        if (activityIndicatorViewColor) refreshControl.activityIndicatorViewColor = [RCTConvert UIColor:activityIndicatorViewColor];
+        
+        [refreshControl addTarget:self action:@selector(dropViewDidBeginRefreshing:) forControlEvents:UIControlEventValueChanged];
+        
+        callback(@[[NSNull null], reactTag]);
+    }];
 }
 
 RCT_EXPORT_METHOD(beginRefreshing:(nonnull NSNumber *)reactTag) {
-  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
-    
-    UIView *view = viewRegistry[reactTag];
-    if (!view) {
-      RCTLogError(@"Cannot find view with tag #%@", reactTag);
-      return;
-    }
-    
-    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
-    
-    ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-      [refreshControl beginRefreshing];
-    });
-  }];
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary *viewRegistry) {
+        
+        UIView *view = viewRegistry[reactTag];
+        if (!view) {
+            RCTLogError(@"Cannot find view with tag #%@", reactTag);
+            return;
+        }
+        
+        UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+        
+        ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [refreshControl beginRefreshing];
+        });
+    }];
 }
 
 RCT_EXPORT_METHOD(endRefreshing:(nonnull NSNumber *)reactTag) {
-  [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, RCTSparseArray *viewRegistry) {
-    
-    UIView *view = viewRegistry[reactTag];
-    if (!view) {
-      RCTLogError(@"Cannot find view with tag #%@", reactTag);
-      return;
-    }
-    
-    UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
-    
-    ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-       [refreshControl endRefreshing];
-    });
-  }];
+    [self.bridge.uiManager addUIBlock:^(RCTUIManager *uiManager, NSDictionary *viewRegistry) {
+        
+        UIView *view = viewRegistry[reactTag];
+        if (!view) {
+            RCTLogError(@"Cannot find view with tag #%@", reactTag);
+            return;
+        }
+        
+        UIScrollView *scrollView = ((RCTScrollView *)view).scrollView;
+        
+        ODRefreshControl *refreshControl = (ODRefreshControl *)[scrollView viewWithTag:[reactTag integerValue]];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [refreshControl endRefreshing];
+        });
+    }];
 }
 
 - (NSDictionary *)constantsToExport {
-  return @{@"UIActivityIndicatorViewStyleWhiteLarge": @(UIActivityIndicatorViewStyleWhiteLarge),
-           @"UIActivityIndicatorViewStyleWhite": @(UIActivityIndicatorViewStyleWhite),
-           @"UIActivityIndicatorViewStyleGray": @(UIActivityIndicatorViewStyleGray)};
+    return @{@"UIActivityIndicatorViewStyleWhiteLarge": @(UIActivityIndicatorViewStyleWhiteLarge),
+             @"UIActivityIndicatorViewStyleWhite": @(UIActivityIndicatorViewStyleWhite),
+             @"UIActivityIndicatorViewStyleGray": @(UIActivityIndicatorViewStyleGray)};
 }
 
 - (void)dropViewDidBeginRefreshing:(ODRefreshControl *)refreshControl {
-  [self.bridge.eventDispatcher sendDeviceEventWithName:@"dropViewDidBeginRefreshing"
-                                                  body:@(refreshControl.tag)];
-  
-  /*
-  double delayInSeconds = 3.0;
-  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-    [refreshControl endRefreshing];
-  }); */
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"dropViewDidBeginRefreshing"
+                                                    body:@(refreshControl.tag)];
+    
+    /*
+     double delayInSeconds = 3.0;
+     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+     [refreshControl endRefreshing];
+     }); */
 }
 
 @end
